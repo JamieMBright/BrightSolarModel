7cbd180da1b2babf5ce0a1acfa638ec025f838b2
Jamie Bright <jamie.bright@anu.edu.au>
2018-05-30 15:16:30 +1000

    removed unneccesray path. updated comments on SDSIG. Added a settigns option for SIG where the cloud sample information can be organised

--- SDSIG-spatio-temporal/utility/SettingsForSDSIG.m
+++ SDSIG-spatio-temporal/utility/SettingsForSDSIG.m
@@ -1,10 +1,11 @@
 
 c_range = 11;
 u_range=25; % the range of windspeeds. standard=60. defined from 'Cloud_Sampling_Technique.m'. This is a length process and, whilst provided, it is recommended to just use the standard.
-num_of_samples=100;% the number of options per windspeed per coverage. standard=1000
+num_of_samples=100;% the number of options per windspeed . standard=1000
 temporal_res=60;
 spatial_res=1500;
-max_num_of_clouds=500;
+max_num_of_clouds=500; % this is used to produce cloud fields and also to reference the cloud fields that are loaded.
+
 panel_pitch_hrz=0; %(degs.) set the panel angle (measured from the horizontal).
 panel_azimuth_hrz=0; %(degs.) set the orientation. measured from north, + to east, - to west. (-180 to 180. +-180=North, 0=south)
 domain_y_max=spatial_res; %maximum size within the y direction. This is a function of the spatial resolution.
---
+++ SIG-temporal_only/supportingfiles/SettingsForSIG.m
@@ -0,0 +1,8 @@
+% There are not many settings fixed for the SIG
+
+%% These setttings are to do with the cloud sample production
+%the range of windspeeds. standard=60 as defined by met office. 
+% this is used to pruduce 1-min cloud coveroptions for up to 60m/s cloud speed
+u_range=60;
+% the number of options per windspeed per coverage. standard=1000. Adaptable only with new cloud sample technique
+num_of_options=1000;

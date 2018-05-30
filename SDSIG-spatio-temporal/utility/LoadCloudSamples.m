8fb1cbb412cfe4ee7f7159ca87c5b0bfe0716176
Jamie Bright <jamie.bright@anu.edu.au>
2018-05-30 12:21:38 +1000

    added some reporting for when the cloud samples will be produced. This will display the basic settings of the system as well as the memory requirements to make these. The raw files cannot be stored into git as they are just too big.

--- SDSIG-spatio-temporal/utility/LoadCloudSamples.m
+++ SDSIG-spatio-temporal/utility/LoadCloudSamples.m
@@ -16,18 +16,23 @@ if exist(name,'file')
     end
 else
     
-    user_input = input('Could not detect the cloud field samples, unzip them within into supporting_files. Want to reproduce? Y/N')
-    if strcmpi(user_input,'Y')
-        % if they dont exist, produce them
-        ProduceVectorCloudFieldSamples
-        
-        disp('Loading store of cloud samples')
-        for i=0:10
-            name=['V2_clouds_vectors_sample',num2str(i),'__temporal_res_',num2str(temporal_res),'__spatial_res_',num2str(spatial_res),'__',num2str(num_of_samples),'_num_of_samples__',num2str(max_num_of_clouds),'max_num_of_clouds'];
-            x=dlmread(fullfile('supportingfiles',name));
-            assignin('base',['cov',num2str(i)],x);
-        end
-    else
-        error('Did not remake cloudfields, try unzipping them as they exist within the git repo')
+    % if they dont exist, produce them
+    disp('The cloud fields did not exist, they will be produced.')
+    disp('  note: this can take some time depending on the SDSIG settings')
+    disp(['  there are ',num2str(u_range),' different wind speeds'])
+    disp(['  there are ',num2str(c_range),' different cloud cover scenarios'])
+    disp(['  there are ',num2str(num_of_samples),' number of cloud samples'])
+    disp(['  each with ',num2str(num_of_clouds),' clouds in each'])
+    disp([' the result is ',num2str(c_range),' files, each of',num2str(u_range*num_of_samples*3*num_of_clouds),' double elements in size'])
+    disp([' the memory demanded is, therefore, ',num2str(c_range*u_range*num_of_samples*3*num_of_clouds*8), ' Bytes'])
+    disp([' each file is ',num2str(u_range*num_of_samples*3*num_of_clouds*8), ' Bytes'])
+    ProduceVectorCloudFieldSamples
+    
+    disp('Loading store of cloud samples')
+    for i=0:10
+        name=['V2_clouds_vectors_sample',num2str(i),'__temporal_res_',num2str(temporal_res),'__spatial_res_',num2str(spatial_res),'__',num2str(num_of_samples),'_num_of_samples__',num2str(max_num_of_clouds),'max_num_of_clouds'];
+        x=dlmread(fullfile('supportingfiles',name));
+        assignin('base',['cov',num2str(i)],x);
     end
+    
 end
